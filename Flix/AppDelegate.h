20efe9f169311184ec53c94606a8de0d81399d71
dianarcodes <94495024+dianarcodes@users.noreply.github.com>
2022-02-17 21:58:18 -0500

    Add files via upload

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by Diana Rivas on 2/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Flix/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by Diana Rivas on 2/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
