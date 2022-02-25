29e9096a774d88c0b868a1a747f0627e5f829289
dianarcodes <94495024+dianarcodes@users.noreply.github.com>
2022-02-24 23:01:04 -0500

    Add files via upload

---
+++ SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Flix
+//
+//  Created by Diana Rivas on 2/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
